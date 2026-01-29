84d7cea4b1e8251f7a911ed4f4a1b87045f86b4c
Thai Ha <thaiha0308au@gmail.com>
2026-01-29 14:15:32 +0700

    Improve badge display and verified label

--- FE/src/app/(pages)/company-manage/cv/detail/[id]/CVDetailClient.tsx
+++ FE/src/app/(pages)/company-manage/cv/detail/[id]/CVDetailClient.tsx
@@ -126,7 +126,7 @@ export const CVDetailClient = ({ cvId, initialCVDetail, initialJobDetail }: CVDe
                     <svg className="w-[14px] h-[14px]" fill="currentColor" viewBox="0 0 20 20">
                       <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd" />
                     </svg>
-                    Verified UIT Student
+                    Verified UIT Student/Alumni
                   </div>
                 )}
               </div>
